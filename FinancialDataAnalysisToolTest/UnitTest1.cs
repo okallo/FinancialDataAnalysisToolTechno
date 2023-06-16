010e48d0e2a276a8f3c564a0ccd4954fb2e4232e
clayne <clayne.co25@gmail.com>
2023-06-16 11:01:59 +0300

    checking diretory

---
+++ FinancialDataAnalysisToolTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace FinancialDataAnalysisToolTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
